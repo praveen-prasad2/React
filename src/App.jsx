d23f6338996083620d88e385c74206d03b018a0b
Praveen-Prasad <praveenprasad2510@gmail.com>
2023-03-02 09:15:55 +0530

    signup form using state

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import MyComponent  from './components/MyComponent'
 // import CardComponent from './components/CardComponent'
 // import CardContainer from './components/CardContainer'
 // import Counter from './components/Counter'
-import LoginPage from './components/LoginPage'
+// import LoginPage from './components/LoginPage'
+import SignUp from './components/SignUp'
 
 function App() {
 
   return (
     <>
-    <LoginPage />
+    <SignUp />
 
     </>
   )
